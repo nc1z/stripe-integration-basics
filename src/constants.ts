729f415b6d005416bd07720916892fb7c6807817
nc1z <ncxwork@gmail.com>
2024-04-03 23:35:19 +0800

    feat: update constants and env

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const PORT = Number(process.env.PORT ?? 3000)
 export const HOST = process.env.HOST ?? "localhost"
+export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY!
+export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET!
--- src/stripe.ts
+++ src/stripe.ts
@@ -1,5 +1,6 @@
 import Stripe from "stripe"
+import { STRIPE_SECRET_KEY } from "./constants"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: "2023-10-16" })
+const stripe = new Stripe(STRIPE_SECRET_KEY, { apiVersion: "2023-10-16" })
 
 export default stripe
